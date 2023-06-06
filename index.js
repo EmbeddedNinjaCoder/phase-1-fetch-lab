2851e6f9a05d29cd4cf8800936737bed8fa6da50
German Santos <santos.german@gmail.com>
2023-06-06 01:27:00 -0700

    fetch lab

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));    
 }
+
+  // // To pass the tests, don't forget to return your fetch!
+  // fetch("https://anapioficeandfire.com/")
+	//   .then(function (response) {
+	//     //console.log(response);
+	//     return response.json();
+	//   })
+	//   .then(renderBooks (data)
+	//     //console.log(data);
+	//   );
+
+
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
